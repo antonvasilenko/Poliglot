00d1938874ff6b457b09b804868c8efb99df72e0
antonv <toxa07@gmail.com>
2014-03-25 01:13:30 +0200

    1: set of basic options added, basic checks applied, skeleton for translator added

--- Poliglot/Program.cs
+++ Poliglot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using NDesk.Options;
@@ -9,8 +10,12 @@ namespace Poliglot
     class Program
     {
         private static string _projectPath;
+        private static string _reportPath;
         private static bool _deleteOldKeys;
         private static bool _promptBeforeDelete;
+        private static string _keyPrefix;
+        private static string _skipMarker;
+        private static int _maxKeyLength;
         private static bool _showHelp;
 
         static void Main(string[] args)
@@ -20,34 +25,88 @@ namespace Poliglot
 
             var lOptionsParser = PrepareOptionsParser();
             ParseStartupOptions(lOptionsParser, args);
-            AnalyzeStartupOptions();
+            var analyzeRes = AnalyzeStartupOptions();
 
-            if (_showHelp)
+            if (!analyzeRes || _showHelp)
             {
-                Console.WriteLine("Usage: Poliglot TODO: add usage sample");
+                Console.WriteLine();
+                Console.WriteLine("Usage: Poliglot --projects:\"C:\\work\\projects.config\" --lang:en,de,ru -d -y -f:\"t__\" -o:out.txt");
+                Console.WriteLine();
                 lOptionsParser.WriteOptionDescriptions(Console.Out);
                 return;
             }
+
+            Console.WriteLine("Searching strings in project(s) folders");
         }
 
         private static OptionSet PrepareOptionsParser()
         {
             var lParser = new OptionSet()
                 {
-                    {"pr|projects=", "path to file where project directories to check are stored", pr => _projectPath = pr},
+                    {"p|projects=", "path to file where project directories to check are stored (file should exist and contain list of paths to folders, one path per line)", pr => _projectPath = pr},
+                    {"l|lang=", "comma-separated string of supported language abbreviations, i.e. en,de,es,ru", d => _deleteOldKeys = d != null},
                     {"d|delkeys", "removes keys from localization files, that no longer present in sources", d => _deleteOldKeys = d != null},
                     {"y|prompt", "prompts to confirm you want to remove this specific old key", y => _promptBeforeDelete = y != null},
-                    {"h|?|help", "shows this help message nad exit", h=> _showHelp = h != null}
+                    {"f|prefix=", "prefix to be added to the key (also a sign whether the string is a key or not)", pr => _keyPrefix = pr},
+                    {"s|skipmarker=", "marker that should be placed before string to ignore it", sm => _skipMarker = sm},
+                    {"m|maxkeylength=", "maximum length of suggested key (generated from original string)", (int len) => _maxKeyLength = len},
+                    {"o|outreport=", "path to file where report will be stored", rep => _reportPath = rep},
+                    {"h|?|help", "shows this help message and exit", h=> _showHelp = h != null}
                 };
             return lParser;
         }
 
-        private static void ParseStartupOptions(OptionSet lOptionsParser, string[] args)
+        private static void ParseStartupOptions(OptionSet lOptionsParser, IEnumerable<string> args)
         {
+            _projectPath = "projects.config";
+            _deleteOldKeys = false;
+            _promptBeforeDelete = true;
+            _keyPrefix = "t_";
+            _skipMarker = "/*skip*/";
+            _maxKeyLength = 50;
+            _showHelp = false;
+
+            List<string> extra;
+            try
+            {
+                extra = lOptionsParser.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("greet: ");
+                Console.WriteLine(e.Message);
+                _showHelp = true;
+            }
         }
 
-        private static void AnalyzeStartupOptions()
+        private static bool AnalyzeStartupOptions()
         {
+            if (_showHelp)
+                return true;
+
+            if (String.IsNullOrEmpty(_projectPath))
+            {
+                Console.WriteLine("path to *.config file not provided, use '-p' or '--projects' option");
+                return false;
+            }
+            if (!File.Exists(_projectPath))
+            {
+                Console.WriteLine("File '{0}' doesn't exists. Check path in '--projects'", _projectPath);
+                return false;
+            }
+
+            if (String.IsNullOrEmpty(_keyPrefix))
+            {
+                Console.WriteLine("Key prefix not set, use '-f' or '--prefix' to define it");
+                return false;
+            }
+
+            if (_maxKeyLength < 20)
+            {
+                Console.WriteLine("Maximum key length ('-m' or '--maxkeylength') cannot be less than 20");
+                return false;
+            }
+            return true;
         }
     }
 }
---
+++ Poliglot/Translator.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Poliglot
+{
+    class Translator
+    {
+        private List<string> _sourceDirs;
+
+        public Translator(IEnumerable<string> sourceDirs)
+        {
+            _sourceDirs = sourceDirs.ToList();
+        }
+
+        private string getRootPath()
+        {
+            if (!String.IsNullOrEmpty(_rootPath))
+                return _rootPath;
+
+            var root = AppDomain.CurrentDomain.BaseDirectory.Replace(@"Mobile\Core\Swissphone.SOne.Mobile.Core.IntTests\bin\Debug", "");
+            return root;
+        }
+
+        public void Run()
+        {
+            var words = getWordsForTranslate();
+
+            var languages = new[] { "en", "de", "ru" };
+            foreach (var language in languages)
+            {
+                updateLanguage(language, words);
+            }
+        }
+
+        private List<string> getWordsForTranslate()
+        {
+            var root = getRootPath();
+            var rootMessages = getWordsFromFolder(root);
+
+            var rawList = rootMessages.Distinct().ToList();
+
+            //from droid
+            var path = Path.Combine(getPathToDroidResource(), "values\\strings.xml");
+            var document = XDocument.Load(path);
+            var droidStrings = document.Descendants("resources").First().Descendants("string");
+            foreach (var droidString in droidStrings)
+            {
+                var key = droidString.Attribute("name").Value;
+                if (!rawList.Contains(key))
+                {
+                    rawList.Add(key);
+                }
+            }
+
+            var list = new List<string>();
+            foreach (var word in rawList.OrderBy(x => x))
+            {
+                if (word.Contains("?") || word.Contains(" ") || word.Contains("!") || word.Contains(".") || word.Contains(","))
+                {
+                    Console.WriteLine("WARNING - " + word);
+                }
+                else
+                {
+                    list.Add(word);
+                }
+            }
+
+            return list;
+        }
+
+        private string getPathToDroidResource()
+        {
+            var path = Path.Combine(getRootPath(), @"Mobile\Droid\Swissphone.SOne.Mobile.Droid.Ui\Resources");
+            return path;
+        }
+
+        private void updateLanguage(string language, IEnumerable<string> words)
+        {
+            var fullList = updateIosDictionary(language, words);
+            replicateToAndroidDictionary(language, fullList);
+        }
+
+        private Dictionary<string, string> updateIosDictionary(string language, IEnumerable<string> words)
+        {
+            var path = Path.Combine(getRootPath(), @"Mobile\Touch\Swissphone.SOne.Mobile.Touch.Ui\Resources");
+            path = Path.Combine(path, language + @".lproj\Localizable.strings");
+
+            var lines = File.ReadAllLines(path, Encoding.UTF8).ToList();
+            var before = lines.Count;
+            foreach (var word in words.OrderBy(x => x))
+            {
+                var left = "\"" + word + "\"";
+                if (lines.Any(x => x.Contains(left)))
+                {
+                    continue;
+                }
+                lines.Add(left + "=\"" + createTranslate(word) + "\";");
+            }
+
+            if (lines.Count > before)
+            {
+                File.WriteAllLines(path, lines.OrderBy(x => x).ToArray());
+            }
+
+            var dictionary = new Dictionary<string, string>();
+            foreach (var line in lines)
+            {
+                var parts = line.Split(new[] { "\"=\"" }, StringSplitOptions.None);
+                var key = parts[0].Trim().Substring(1);
+                var value = parts[1].Trim().Substring(0, parts[1].Length - 2);
+                dictionary.Add(key, value);
+            }
+            return dictionary;
+        }
+
+        private void replicateToAndroidDictionary(string language, Dictionary<string, string> words)
+        {
+            if (language == "en")
+            {
+                language = "";
+            }
+            else
+            {
+                language = "-" + language;
+            }
+            var path = Path.Combine(getPathToDroidResource(), "values" + language + @"\strings.xml");
+
+            var document = XDocument.Load(path);
+            var resource = document.Descendants("resources").First();
+
+            foreach (var pair in words)
+            {
+                var element = resource.XPathSelectElement("string[@name = \"" + pair.Key + "\"]");
+                if (element == null)
+                {
+                    element = new XElement("string", "");
+                    element.SetAttributeValue("name", pair.Key);
+                    resource.Add(element);
+                }
+                element.SetValue(pair.Value.Replace("'", "\\'"));
+            }
+
+            document.Save(path);
+        }
+
+        private string createTranslate(string val)
+        {
+            return "-" + RemoveSpecialCharacters(val, true);
+        }
+
+        private IEnumerable<string> getWordsFromFolder(string path)
+        {
+            return Directory
+                .GetFiles(path, "*.cs", SearchOption.AllDirectories)
+                .Select(getWordsFromFile)
+                .SelectMany(x => x)
+                .ToList();
+        }
+
+        private List<string> getWordsFromFile(string path)
+        {
+            var text = File.ReadAllText(path);
+            var matches = Regex.Matches(text, "\"t_(.*?)\"");
+
+            var list = new List<string>();
+            for (var i = 0; i < matches.Count; i++)
+            {
+                var val = matches[i].Value;
+                if (val.Length > 10)
+                {
+                    val = val.Replace("\"", "");
+                    list.Add(val);
+                }
+            }
+
+            return list;
+        }
+
+        public static string RemoveSpecialCharacters(string text, bool skipLogger = false)
+        {
+            if (string.IsNullOrEmpty(text) || text.Length <= 3)
+            {
+                return text;
+            }
+
+            var prefixEndPos = text.IndexOf('_', 2);
+            if (prefixEndPos >= 0)
+            {
+                if (!skipLogger)
+                {
+                    Console.WriteLine("Text [{0}] doesn't have translate.", text);
+                }
+
+                return text.Substring(prefixEndPos + 1, text.Length - 1 - prefixEndPos).Replace("_", " ");
+            }
+
+            return text;
+        }
+
+    }
+}
